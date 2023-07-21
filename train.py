6b5ed2e9c4fc29e30889ab359dcd408840309cf5
KimRass <rmx1000@yonsei.ac.kr>
2023-07-21 14:25:35 +0900

    Local to server

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ N_WORKERS = 4
 
 DEVICE = get_device()
 gen = Generator().to(DEVICE)
-torch.save(gen.state_dict(), f"""/home/ubuntu/project/dcgan_from_scratch/parameters/text.pth""")
+torch.save(gen.state_dict(), "/home/ubuntu/project/dcgan_from_scratch/parameters/text.pth")
 
 # "We used the Adam optimizer with tuned hyperparameters. We used 0.0002 for learning rate. We found
 # reducing $\beta_{1}$ to 0.5 helped stabilize training."
